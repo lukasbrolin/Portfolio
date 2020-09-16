1bf71867736747088d986c13ca4eaf8b386366cd
lukasbrolin <lukas.brolin@live.se>
2020-09-16 22:58:49 +0200

    js content added

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,1 +1,18 @@
-//Hej
+//hej
+
+$(document).ready(function(){
+
+    window.addEventListener('scroll', function(){scrollWindow()},false);
+
+    var scrollPos = 0;
+
+    function scrollWindow(){
+        if (((document.body.getBoundingClientRect()).top < scrollPos) && scrollPos === 0 ){
+            $('html,body').animate({
+                scrollTop: $(".content").offset().top},
+                'slow');
+        }
+        // saves the new position for iteration
+        scrollPos = (document.body.getBoundingClientRect()).top;
+    }
+})
