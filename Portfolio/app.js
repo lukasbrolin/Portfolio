f935edcb6a0ee5b72f31625bcdf3575a8f585401
lukasbrolin <lukas.brolin@live.se>
2020-09-16 23:19:06 +0200

    Update app.js

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -12,6 +12,11 @@ $(document).ready(function(){
                 scrollTop: $(".content").offset().top},
                 'slow');
         }
+        else if((document.body.getBoundingClientRect()).top > scrollPos && (scrollPos + document.getElementById("backgroundHeader").clientHeight ) === 0){
+            $('html,body').animate({
+                scrollTop: $(".header").offset().top},
+                'slow');
+        }
         // saves the new position for iteration
         scrollPos = (document.body.getBoundingClientRect()).top;
     }
