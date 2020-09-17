8f6c182f3665dfaa66b620d75f4547b0412306b8
pandrum <simon.bernsdorff@outlook.com>
2020-09-17 11:18:30 +0200

    awyeah

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,23 +1,39 @@
 //hej1
 
-$(document).ready(function(){
+$(document).ready(function () {
+  window.addEventListener(
+    "scroll",
+    function () {
+      scrollWindow();
+    },
+    false
+  );
 
-    window.addEventListener('scroll', function(){scrollWindow()},false);
+  var scrollPos = 0;
 
-    var scrollPos = 0;
-
-    function scrollWindow(){
-        if (((document.body.getBoundingClientRect()).top < scrollPos) && scrollPos === 0 ){
-            $('html,body').animate({
-                scrollTop: $(".content").offset().top},
-                'slow');
-        }
-        else if((document.body.getBoundingClientRect()).top > scrollPos && (scrollPos + document.getElementById("backgroundHeader").clientHeight ) === 0){
-            $('html,body').animate({
-                scrollTop: $(".header").offset().top},
-                'slow');
-        }
-        // saves the new position for iteration
-        scrollPos = (document.body.getBoundingClientRect()).top;
+  function scrollWindow() {
+    if (
+      document.body.getBoundingClientRect().top < scrollPos &&
+      scrollPos === 0
+    ) {
+      $("html,body").animate(
+        {
+          scrollTop: $(".content").offset().top,
+        },
+        "slow"
+      );
+    } else if (
+      document.body.getBoundingClientRect().top > scrollPos &&
+      scrollPos + document.getElementById("backgroundHeader").clientHeight === 0
+    ) {
+      $("html,body").animate(
+        {
+          scrollTop: $(".header").offset().top,
+        },
+        "slow"
+      );
     }
-})
+    // saves the new position for iteration
+    scrollPos = document.body.getBoundingClientRect().top;
+  }
+});
