cc4b18339a736bd6bbbc575ec8a3ba8fd8d4ace1
pandrum <simon.bernsdorff@outlook.com>
2020-09-18 13:24:08 +0200

    utkommenterad scropp

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -2,55 +2,62 @@ $(function () {
   window.addEventListener(
     "scroll",
     function () {
-      scrollWindow()
+      // scrollWindow();
       navBarFunction();
     },
     false
   );
 
-  var scrollPos = 0;
-
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
-  function navBarFunction(){
-    
-    if(document.body.getBoundingClientRect().top + document.getElementById("backgroundHeader").clientHeight < 0){
-      navbar.classList.add("sticky")
-      console.log("success")
-    }
-    else if((document.body.getBoundingClientRect().top + document.getElementById("backgroundHeader").clientHeight >= 0)){
-      navbar.classList.remove("sticky")
-      console.log("fail")
-    }
-
-  }
-
-  function scrollWindow() {
-    if (document.body.getBoundingClientRect().top < scrollPos &&
-      scrollPos === 0
+  function navBarFunction() {
+    if (
+      document.body.getBoundingClientRect().top +
+        document.getElementById("backgroundHeader").clientHeight <
+      0
     ) {
-      $("html,body").animate(
-        {
-          scrollTop: $(".contentTitle").offset().top,
-        },
-        "slow"
-      );
+      navbar.classList.add("sticky");
+      console.log("success");
     } else if (
-      document.body.getBoundingClientRect().top > scrollPos &&
-      scrollPos + (document.getElementById("backgroundHeader").clientHeight + 196) >
-        -10 &&
-      scrollPos + (document.getElementById("backgroundHeader").clientHeight + 196) < 150
+      document.body.getBoundingClientRect().top +
+        document.getElementById("backgroundHeader").clientHeight >=
+      0
     ) {
-      $("html,body").animate(
-        {
-          scrollTop: $("body").offset().top,
-        },
-        "slow"
-      );
+      navbar.classList.remove("sticky");
+      console.log("fail");
     }
-    // saves the new position for iteration
-    scrollPos = document.body.getBoundingClientRect().top;
   }
+
+  // function scrollWindow() {
+  //   if (
+  //     document.body.getBoundingClientRect().top < scrollPos &&
+  //     scrollPos === 0
+  //   ) {
+  //     $("html,body").animate(
+  //       {
+  //         scrollTop: $(".contentTitle").offset().top,
+  //       },
+  //       "slow"
+  //     );
+  //   } else if (
+  //     document.body.getBoundingClientRect().top > scrollPos &&
+  //     scrollPos +
+  //       (document.getElementById("backgroundHeader").clientHeight + 196) >
+  //       -10 &&
+  //     scrollPos +
+  //       (document.getElementById("backgroundHeader").clientHeight + 196) <
+  //       150
+  //   ) {
+  //     $("html,body").animate(
+  //       {
+  //         scrollTop: $("body").offset().top,
+  //       },
+  //       "slow"
+  //     );
+  //   }
+  //   // saves the new position for iteration
+  //   scrollPos = document.body.getBoundingClientRect().top;
+  // }
 
   // Hamburger
   var hamburger = document.querySelector(".hamburger");
