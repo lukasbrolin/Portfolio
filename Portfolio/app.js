660ca3ad0c9acfc2d431aa62f9b2ea0c78f7d712
pandrum <simon.bernsdorff@outlook.com>
2020-09-22 13:19:29 +0200

    WOOOO

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,81 +1,15 @@
-$(document).ready(function () {
-  var members = [
-    {
-      name: "Lukas Brolin",
-      introText: "Hej jag heter Lukas",
-      chartLabel: ["PS", "Java", "HTML", "CSS", "JS"],
-      chartValue: ["7", "7", "7", "6", "6"],
-      image: "img/Lukas.jpg",
-      location: "Västerås, SE",
-      email: "lukas@oru.se",
-      phone: "+46 707 818 096",
-    },
-    {
-      name: "Simon Wallstedt Bernsdorff",
-      introText: "Hej jag heter Simon",
-      chartLabel: ["PS", "Java", "HTML", "CSS", "JS", "Kuksugning"],
-      chartValue: ["7", "7", "7", "6", "6", "10"],
-      image: "img/SimonTest.jpg",
-      location: "Örebro, SE",
-      email: "simon@oru.se",
-      phone: "0202020250",
-    },
-    {
-      name: "Jonatan Angergård",
-      introText: "Hej jag heter Jonatan",
-      chartLabel: ["PS", "Java", "HTML", "CSS", "JS"],
-      chartValue: ["7", "7", "7", "6", "6"],
-      image: "img/Jonatan.png",
-      location: "Örebro, SE",
-      email: "jonte@oru.se",
-      phone: "020202040",
-    },
-    {
-      name: "Sven Isac Hedengren",
-      introText: "Hej jag heter Isac",
-      chartLabel: ["PS", "Java", "HTML", "CSS", "JS"],
-      chartValue: ["1", "10", "5", "2", "6"],
-      image: "img/Isac.png",
-      location: "Örebro, SE",
-      email: "isac@oru.se",
-      phone: "020202030",
-    },
-  ];
+$(function () {
+  $(".mainCarousel").flickity({
+    cellAlign: "center",
+    wrapAround: true,
+    // freeScroll: true
+  });
 
-  console.log(sessionStorage.getItem("number"));
-  var getIndex = sessionStorage.getItem("number");
-  var indexBefore = getIndex.substring(getIndex.length - 1);
-  var clickedAnchor = "";
-  var contentAnchors = document.getElementsByClassName("contentAnchor");
-  console.log(contentAnchors);
-  for (i = 0; i < contentAnchors.length; i++) {
-    console.log("hej");
-    //document.
-    contentAnchors[i].addEventListener("click", function (e) {
-      sessionStorage.setItem("number", this);
-      clickedAnchor = e;
-      console.log(e);
-    });
-  }
+  $(".projectCarousel").flickity({
+    cellAlign: "center",
+    wrapAround: true,
+  });
 
-  
-
-   function setValues(theValue){
-     console.log("saywut")
-    document.getElementById("memberIntro").innerHTML = "<p><h1>" + members[theValue].name + "</h1>" + members[theValue].introText + "</p>"
-    document.getElementById("memberName").innerHTML = "<Strong>Name: </Strong>" + members[theValue].name
-    document.getElementById("memberLocation").innerHTML = "<Strong>Location: </Strong>" + members[theValue].location
-    document.getElementById("memberMail").innerHTML = "<Strong>Email: </Strong>" + members[theValue].ematheValue
-    document.getElementById("memberPhone").innerHTML = "<Strong>Phone: </Strong>" + members[theValue].phone
-    console.log("yeeee")
-   }
-
-   
-   sessionStorage.setItem("number", "")
-   LoadChart(indexBefore);
-  //  setValues(indexBefore)
-
-  
   window.addEventListener(
     "scroll",
     function () {
@@ -90,43 +24,28 @@ $(document).ready(function () {
     if (
       document.body.getBoundingClientRect().top +
         document.getElementById("backgroundHeader").clientHeight <=
-        0
-        ) {
-          navbar.classList.add("sticky");
-        } else if (
-          document.body.getBoundingClientRect().top +
-          document.getElementById("backgroundHeader").clientHeight >
-          0
-          ) {
-            navbar.classList.remove("sticky");
-          }
-        }
+      0
+    ) {
+      navbar.classList.add("sticky");
+    } else if (
+      document.body.getBoundingClientRect().top +
+        document.getElementById("backgroundHeader").clientHeight >
+      0
+    ) {
+      navbar.classList.remove("sticky");
+    }
+  }
 
-        function removeData(chart) {
-          chart.data.labels.pop();
-          chart.data.datasets.forEach((dataset) => {
-              dataset.data.pop();
-          });
-          chart.update();
-      }
-        
-        // Charts
-        function LoadChart(i){
-          console.log(i)
-          if(i === ""){
-            i=0;
-          }
-          
+  // Charts
   var ctx = document.getElementById("myChart").getContext("2d");
   var myChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels: members[i].chartLabel,
-      // labels: ["PS", "Java", "HTML", "CSS", "JS"],
+      labels: ["PS", "Java", "HTML", "CSS", "JS"],
       datasets: [
         {
           label: "Expertise",
-          data: members[i].chartValue,
+          data: [7, 8, 7, 6, 5],
           backgroundColor: [
             "rgba(163, 162, 152,1)",
             "rgba(99,99,92,1)",
@@ -157,112 +76,18 @@ $(document).ready(function () {
       scales: {
         yAxes: [
           {
-            label: "Expertise",
-            data: members[i].chartValue,
-            backgroundColor: [
-              "rgba(163, 162, 152,1)",
-              "rgba(99,99,92,1)",
-              "rgba(163, 162, 152,1)",
-              "rgba(99,99,92,1)",
-              "rgba(163, 162, 152,1)",
-              "rgba(99,99,92,1)",
-            ],
-            borderColor: [
-              "rgba(163, 162, 152,1)",
-              "rgba(99,99,92,1)",
-              "rgba(163, 162, 152,1)",
-              "rgba(99,99,92,1)",
-              "rgba(163, 162, 152,1)",
-              "rgba(99,99,92,1)",
-            ],
-            borderWidth: 1,
+            ticks: {
+              beginAtZero: true,
+              max: 10,
+            },
           },
         ],
       },
-      options: {
-        legend: {
-          display: true,
-          labels: {
-            boxWidth: 0,
-          },
-        },
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                max: 10,
-              },
-            },
-          ],
-        },
-      },
-    });
-  }
-
-function setValues(indexBefore){
-  if(indexBefore === ""){
-    indexBefore=0;
-  }
-  console.log("saywut")
-  console.log(members[1])
-  console.log(indexBefore + "hej")
- document.getElementById("memberIntro").innerHTML = "<p><h1>" + members[indexBefore].name + "</h1>" + members[indexBefore].introText + "</p>"
- document.getElementById("memberName").innerHTML = "<Strong>Name: </Strong>" + members[indexBefore].name
- document.getElementById("memberLocation").innerHTML = "<Strong>Location: </Strong>" + members[indexBefore].location
- document.getElementById("memberMail").innerHTML = "<Strong>Email: </Strong>" + members[indexBefore].ematheValue
- document.getElementById("memberPhone").innerHTML = "<Strong>Phone: </Strong>" + members[indexBefore].phone
- console.log("yeeee")
-}
-
-setValues(indexBefore)
-
-  var carouselIndex = 0;
-  var $carousel = $(".mainCarousel").flickity({
-    cellAlign: "center",
-    wrapAround: true,
-    on: {
-      change: function (index) {
-        document.getElementById("memberIntro").innerHTML =
-          "<p><h1>" +
-          members[index].name +
-          "</h1>" +
-          members[index].introText +
-          "</p>";
-        document.getElementById("memberName").innerHTML =
-          "<Strong>Name: </Strong>" + members[index].name;
-        document.getElementById("memberLocation").innerHTML =
-          "<Strong>Location: </Strong>" + members[index].location;
-        document.getElementById("memberMail").innerHTML =
-          "<Strong>Email: </Strong>" + members[index].email;
-        document.getElementById("memberPhone").innerHTML =
-          "<Strong>Phone: </Strong>" + members[index].phone;
-        carouselIndex = index;
-        // myChart.options.labels.pop();
-        // removeData(myChart)
-        // function updateConfigByMutating(chart) {
-          // myChart.data.labels = "hej";
-          // myChart.data.datasets[0] = 6
-          // chart.update();
-      // }
-        LoadChart(carouselIndex);
-        // myChart.update();
-        console.log(members[carouselIndex].chartLabel);
-      },
     },
-    // freeScroll: true
   });
-
-   $carousel.flickity( 'select', indexBefore );
-  
 
   var element = document.querySelector(".main-carousel");
   console.log(element);
-
-  function getInstance() {
-    var flkty = $carousel.data("flickity");
-    console.log(flkty.selectedIndex);
-  }
 
   // google.charts.load('current', {'packages':['bar']});
   //     google.charts.setOnLoadCallback(drawStuff);
@@ -298,6 +123,25 @@ setValues(indexBefore)
   //       var chart = new google.charts.Bar(document.getElementById('chart'));
   //       chart.draw(data, options);
   //     };
+
+  // var members = [
+  //   {
+  //     name: "Lukas Brolin",
+  //     image: "img/Lukas.jpg",
+  //   },
+  //   {
+  //     name: "Simon Wallstedt Bernsdorff",
+  //     image: "img/SimonTest.jpg",
+  //   },
+  //   {
+  //     name: "Jonatan Angergård",
+  //     image: "img/Jonatan.png",
+  //   },
+  //   {
+  //     name: "Sven Isac Hedengren",
+  //     image: "img/Isac.png",
+  //   },
+  // ];
 
   // // var queryString = location.search.substring(1);
 
@@ -387,4 +231,3 @@ setValues(indexBefore)
 
   navBarFunction();
 });
-window.onload = function () {};
