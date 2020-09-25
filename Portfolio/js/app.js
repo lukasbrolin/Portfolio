5fc7c3705c28922ca2a3b2d5b3887d078b738d7c
lukasbrolin <lukas.brolin@live.se>
2020-09-25 13:14:08 +0200

    xd

--- Portfolio/js/app.js
+++ Portfolio/js/app.js
@@ -1,4 +1,73 @@
 $(document).ready(function () {
+
+
+    document.getElementById("theScroll").addEventListener("click", function (e) {
+      e.preventDefault();
+        scrollWindow();
+        // navBarFunction()
+        // scrollWindow()
+        // navBarFunction();
+      },
+      false
+    );
+  
+    var scrollPos = 0;
+  
+    // var navbar = document.getElementById("navbar");
+    // var sticky = navbar.offsetTop;
+    // function navBarFunction(){
+  
+    //   if(document.body.getBoundingClientRect().top + document.getElementById("backgroundHeader").clientHeight < 0){
+    //     navbar.classList.add("sticky")
+    //   }
+    //   else if((document.body.getBoundingClientRect().top + document.getElementById("backgroundHeader").clientHeight >= 0)){
+    //     navbar.classList.remove("sticky")
+    //   }
+  
+    // }
+
+    function scrollWindow() {
+      console.log("Vad detta nu är: " + document.body.getBoundingClientRect().top);
+      console.log("Ska vara samma som ovan ^: " + window.scrollY)
+      console.log("ScrollPos: " + scrollPos);
+      console.log("Background + navbar height: " + (document.getElementById("backgroundHeader").clientHeight + document.getElementById("navbar").clientHeight));
+      if (window.scrollY < document.getElementById("backgroundHeader").clientHeight
+        // document.body.getBoundingClientRect().top < scrollPos &&
+        // (document.body.getBoundingClientRect().top <= 0 && document.body.getBoundingClientRect().top > -50)
+      ) {
+        console.log("DU FÅR INTE KOMMA IVÄG");
+        $("html,body").animate(
+          {
+            scrollTop: $("#membersTitle").offset().top,
+          },
+          "slow", "linear"
+        );
+      } else if (window.scrollY >= document.getElementById("backgroundHeader").clientHeight
+        // document.body.getBoundingClientRect().top > scrollPos &&
+        // scrollPos +
+        //   // (document.getElementById("backgroundHeader").clientHeight + 196) >
+        //   (document.getElementById("backgroundHeader").clientHeight + document.getElementById("navbar").clientHeight) >
+        //   -10 &&
+        // scrollPos +
+        //   // (document.getElementById("backgroundHeader").clientHeight + 196) <
+        //   (document.getElementById("backgroundHeader").clientHeight + document.getElementById("navbar").clientHeight) <
+        //   150
+      ) {
+        $("html,body").animate(
+          {
+            scrollTop: $("body").offset().top,
+          },
+          "slow"
+        );
+      }
+      else{
+        console.log("jag skiter i dig");
+      }
+      // saves the new position for iteration
+      scrollPos = document.body.getBoundingClientRect().top;
+    }
+ 
+  
   $(".projectCarouselInteraktion").flickity({
     cellAlign: "center",
     wrapAround: true,
@@ -119,6 +188,10 @@ $(document).ready(function () {
       navbar.classList.remove("sticky");
     }
   }
+// new
+  
+
+// to new
 
   function removeData(chart) {
     chart.data.labels.pop();
