ecf11754a37e042b88a59f8e070d80d9f3f8f789
lukasbrolin <lukas.brolin@live.se>
2020-09-25 14:39:57 +0200

    xd

--- Portfolio/js/member.js
+++ Portfolio/js/member.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
       introText:
         "I am a programmer. You think thats shit funny? It's a hard and tedious task that only the best can do, and I am the best at what I do. I used to work for NASA and CERN before I got bored and decided to challenge my intellect even further and switched to HTML/CSS and JavaScript. I never test my code. Testing your code is just synonomous with being a bad programmer that fails at life. I write my code once and then it works, simple as that.",
       chartLabel: ["PS", "Java", "HTML", "CSS", "JS", "XD", "Sketchup"],
-      chartValue: ["7", "8", "7", "6", "7", "8", "5"],
+      chartValue: ["7", "8", "7", "6", "7", "8", "7"],
       image: "img/Lukas.jpg",
       location: "Västerås, SE",
       email: "lukas@oru.se",
@@ -30,8 +30,8 @@ $(document).ready(function () {
       name: "Simon Wallstedt Bernsdorff",
       introText:
         "Once every blue moon I write some code, if Lukas allows it. He is usually the boss around here, even though me and the other guys see him more as a dictator/fascist. Someday I wish I was as good as Lukas, just having that go in me and being a true rockstar programmer. For now I'll just stick to being lambasted by Lukas and commenting code which is the only thing I am allowed to do. Oh, and I also make coffee for the team. Gotta start somewhere y'know?",
-      chartLabel: ["PS", "Java", "HTML", "CSS", "JS"],
-      chartValue: ["1", "1", "2", "1", "0.5"],
+      chartLabel: ["PS", "Java", "HTML", "CSS", "JS", "Coffee making"],
+      chartValue: ["1", "1", "0", "1", "0.5", "1.5"],
       image: "img/SimonTest.jpg",
       location: "Örebro, SE",
       email: "simon@oru.se",
@@ -53,7 +53,7 @@ $(document).ready(function () {
       introText:
         "Hej jag heter Isac. Detta är en beskrivande text, i enlighet med projektuppgift.",
       chartLabel: ["PS", "Java", "HTML", "CSS"],
-      chartValue: ["2", "4", "4", "4"],
+      chartValue: ["2", "7", "8", "7"],
       image: "img/Isac.png",
       location: "Örebro, SE",
       email: "isac@oru.se",
